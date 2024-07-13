d4e523fdc1f95bdc7e271ad8d002a857525f81b1
viram singh <sisodiyaviramsingh415@gmail.com>
2024-07-13 16:58:51 +0530

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+// import viteLogo from "/vite.svg";
 import "./App.css";
 import { TodoContext, TodoProvider } from "./contexts";
 import TodoForm from "./components/TodoForm";
